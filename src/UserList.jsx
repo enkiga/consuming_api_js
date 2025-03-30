e80fe28407e99c397c465968a26b25479ef32e14
Enoch Garoli <eenkiga@gmail.com>
2025-03-30 13:11:14 +0300

    Tailwind Styling

--- src/UserList.jsx
+++ src/UserList.jsx
@@ -1,6 +1,6 @@
 // src/UserList.js
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const UserList = () => {
   const [listOfUsers, setListOfUsers] = useState([]);
@@ -8,10 +8,12 @@ const UserList = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        const response = await axios.get(
+          "https://jsonplaceholder.typicode.com/users"
+        );
         setListOfUsers(response.data);
       } catch (error) {
-        console.error('Error fetching users:', error);
+        console.error("Error fetching users:", error);
       }
     };
 
@@ -19,19 +21,26 @@ const UserList = () => {
   }, []);
 
   return (
-    <div className="user-list-container">
-      <h1>List of Users</h1>
-      <div className="users-grid">
-        {listOfUsers.map(user => (
-          <div key={user.id} className="user-card">
-            <h2>{user.name}</h2>
-            <p>📧 {user.email}</p>
-            <p>📱 {user.phone}</p>
-            <p>🌐 {user.website}</p>
-            <div className="user-address">
-              <h4>Address:</h4>
-              <p>{user.address.street}, {user.address.suite}</p>
-              <p>{user.address.city}, {user.address.zipcode}</p>
+    <div className="w-full  bg-gray-100 p-4">
+      <h1 className="text-center text-2xl font-bold py-3">List of Users</h1>
+      <div className="flex flex-wrap justify-starts-center">
+        {listOfUsers.length === 0 && (
+          <div className="text-center text-gray-500">Loading...</div>
+        )}
+        {listOfUsers.map((user) => (
+          <div key={user.id} className="w-full md:w-1/4 lg:w-1/4 p-4 border">
+            <h2 className="font-semibold text-xl pb-2">{user.name}</h2>
+            <p className="text-blue-400 text-sm ">📧 {user.email}</p>
+            <p className=" tracking-wide font-semibold">📱 {user.phone}</p>
+            <p className="text-blue-400 text-sm ">🌐 {user.website}</p>
+            <div className="mt-4">
+              <h4 className="font-semibold">📍 Address:</h4>
+              <p className="text-sm hover:cursor-pointer hover:text-blue-500 hover:font-semibold">
+                {user.address.street}, {user.address.suite}
+              </p>
+              <p className="text-sm hover:cursor-pointer hover:text-blue-500 hover:font-semibold">
+                {user.address.city}, {user.address.zipcode}
+              </p>
             </div>
           </div>
         ))}
